65ed81cfda2615ad75a1cd556301dbba7cfeca35
User <user@example.com>
2026-03-03 23:50:25 +0530

    Update Firebase config with new project details

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCxGqZK03fi--8tqk42WdK_1Ie-fbc465U",
-    authDomain: "nsuthackathon.firebaseapp.com",
-    projectId: "nsuthackathon",
-    storageBucket: "nsuthackathon.firebasestorage.app",
-    messagingSenderId: "971787825936",
-    appId: "1:971787825936:web:b8888654a7c12edf49eb95",
-    measurementId: "G-NNV27VFM32"
+    apiKey: "AIzaSyDnt41Jwppp-rvbDNl6GOPEndHoEjHsGmg",
+    authDomain: "nagarcycle-3710f.firebaseapp.com",
+    projectId: "nagarcycle-3710f",
+    storageBucket: "nagarcycle-3710f.firebasestorage.app",
+    messagingSenderId: "917905120543",
+    appId: "1:917905120543:web:0588615b729f8fab4f242a",
+    measurementId: "G-5Y6X3D0SM0"
 };
 
 // Initialize Firebase
